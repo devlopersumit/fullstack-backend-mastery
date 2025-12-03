6d38a30e053a32c8961ecd572d085255ac520801
devlopersumit <sumitsj2002@gmail.com>
2025-12-03 21:49:05 +0530

    connected database successfully

--- beginner-url-shortener/server/app.js
+++ beginner-url-shortener/server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//DB Connection
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
