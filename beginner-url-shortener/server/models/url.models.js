6b08dd4cb5a867b17d35ac9735bd1e59bcad23de
devlopersumit <sumitsj2002@gmail.com>
2025-12-04 22:44:00 +0530

    created url model for url shortener

---
+++ beginner-url-shortener/server/models/url.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+    originalUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    shortId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    clicks:{
+        type:Number,
+        default:0
+    },
+    createdAt: {
+        type:Date,
+        default:Date.now()
+    }
+});
+
+const URL = mongoose.model('URL', urlSchema);
